99f67ac9cb8c118569813fe93550cb139f97f42c
John Ky <john.ky@iohk.io>
2022-09-24 23:26:12 +1000

    stuff

--- cardano-node-chairman/app/Cardano/Chairman/Commands/Run.hs
+++ cardano-node-chairman/app/Cardano/Chairman/Commands/Run.hs
@@ -14,6 +14,7 @@ import           Control.Tracer (Tracer (..), stdoutTracer)
 import qualified Data.Text as Text
 import qualified Data.Time.Clock as DTC
 import           Options.Applicative
+import qualified Options.Applicative as Opt
 import qualified System.IO as IO
 
 import           Cardano.Node.Configuration.NodeAddress
@@ -63,7 +64,7 @@ parseSocketPath helpMessage =
 
 parseRunningTime :: Parser DiffTime
 parseRunningTime =
-  option ((fromIntegral :: Int -> DiffTime) <$> auto)
+  Opt.option ((fromIntegral :: Int -> DiffTime) <$> auto)
     (  long "timeout"
     <> short 't'
     <> metavar "SECONDS"
@@ -72,7 +73,7 @@ parseRunningTime =
 
 parseProgress :: Parser BlockNo
 parseProgress =
-  option ((fromIntegral :: Int -> BlockNo) <$> auto)
+  Opt.option ((fromIntegral :: Int -> BlockNo) <$> auto)
     (  long "require-progress"
     <> short 'p'
     <> metavar "INT"
